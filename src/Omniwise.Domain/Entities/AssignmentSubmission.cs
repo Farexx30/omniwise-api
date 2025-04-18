dfa9d1972ba6a348d7b3f9187850bc604edc8473
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-18 13:16:51 +0200

    Feature: AssignmentSubmission entity added.

---
+++ src/Omniwise.Domain/Entities/AssignmentSubmission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Domain.Entities;
+
+public class AssignmentSubmission
+{
+    public int Id { get; set; }
+    public float? Grade { get; set; }
+    public DateTime LatestSubmissionDate { get; set; }
+    public int AssignmentId { get; set; }
+    public string AuthorId { get; set; } = default!;
+}
