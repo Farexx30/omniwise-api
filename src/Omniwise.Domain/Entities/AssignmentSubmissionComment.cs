3df79cd29e12a84a931ad160650180b54312fb12
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-18 13:18:05 +0200

    Feature: AssignmentSubmissionComment entity added.

---
+++ src/Omniwise.Domain/Entities/AssignmentSubmissionComment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Domain.Entities;
+
+public class AssignmentSubmissionComment
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = default!;
+    public DateTime SentDate { get; set; }
+    public int AssignmentSubmissionId { get; set; }
+    public string AuthorId { get; set; } = default!;
+}
