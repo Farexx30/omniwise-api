4401ef70f39b2a1793e9fd5422478544968d7f90
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-18 13:15:26 +0200

    Feature: Assignment entity added.

---
+++ src/Omniwise.Domain/Entities/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Domain.Entities;
+
+public class Assignment
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string? Content { get; set; }
+    public DateTime Deadline { get; set; }
+    public float MaxGrade { get; set; }
+    public int CourseId { get; set; }
+}
