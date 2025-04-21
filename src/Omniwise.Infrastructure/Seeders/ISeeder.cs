84b3c06685c32f525f0f3d1a914c7f1b2fd47010
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-21 19:58:30 +0200

    Feature: Generic ISeeder interface and Role seeder with Roles constants.

---
+++ src/Omniwise.Infrastructure/Seeders/ISeeder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Infrastructure.Seeders;
+
+public interface ISeeder<TEntity>
+    where TEntity : class
+{
+    Task SeedAsync();
+}
---
+++ src/Omniwise.Infrastructure/Seeders/RoleSeeder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Omniwise.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Infrastructure.Seeders;
+
+internal class RoleSeeder(RoleManager<IdentityRole> roleManager) : ISeeder<IdentityRole>
+{
+    private readonly IEnumerable<string> _roleNames = [Roles.Admin, 
+        Roles.Teacher, 
+        Roles.Student];
+
+    public async Task SeedAsync()
+    {
+        foreach (var roleName in _roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                var role = CreateRole(roleName);
+                await roleManager.CreateAsync(role);
+            }
+        }
+    }
+
+    private static IdentityRole CreateRole(string name)
+    {
+        return new IdentityRole
+        {
+            Name = name,
+            NormalizedName = name.ToUpper()
+        };
+    }  
+}
