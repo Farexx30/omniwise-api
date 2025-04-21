535f1e44ead2691cdbac9872aa1ca4a0d9d562d6
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-21 17:20:44 +0200

    Feature: Custom identity register endpoint with proper validation tailored to this app.

---
+++ src/Omniwise.Application/Identity/Requests/OmniwiseRegisterRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Application.Identity.Requests;
+
+/// <summary>
+/// The request type for the "/register" endpoint added by <see cref="IdentityApiEndpointRouteBuilderExtensions.MapOmniwiseIdentityApi"/>.
+/// </summary>
+public sealed class OmniwiseRegisterRequest
+{
+    //Properties from the original RegisterRequest class:
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+
+    //Custom properties:
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string RoleName { get; init; }
+}
