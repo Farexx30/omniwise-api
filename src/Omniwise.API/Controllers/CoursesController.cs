c671f6fb83858d460a9d26eee04887d4717c1b07
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-24 12:12:34 +0200

    Feature: Installed neccessary packages in application layer, created CoursesController and added CreateCourse endpoint with CreateCourseCommand.

---
+++ src/Omniwise.API/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Omniwise.Application.Courses.Commands.CreateCourse;
+using Omniwise.Domain.Constants;
+
+namespace Omniwise.API.Controllers;
+
+[ApiController]
+[Route("api/courses")]
+public class CoursesController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [Authorize(Roles = Roles.Teacher)]
+    public async Task<IActionResult> CreateCourse([FromBody] CreateCourseCommand command)
+    {
+        int id = await mediator.Send(command);
+
+        return CreatedAtAction(/*nameof(GetCourseById)*/ null, new { id }, null); //First argument waits for the GetCourseById action to exist.
+    }
+}
---
+++ src/Omniwise.Application/Courses/Commands/CreateCourse/CreateCourseCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Application.Courses.Commands.CreateCourse;
+
+public class CreateCourseCommand : IRequest<int>
+{
+    public required string Name { get; init; }
+    public string? ImgUrl { get; init; }
+}
