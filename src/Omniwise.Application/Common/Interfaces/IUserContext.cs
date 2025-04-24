9c964eba5d6e0ec98a8bae738458159fe8bd4076
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-24 19:49:05 +0200

    Feature and refactor: Updating course with authorization. UserContext adjustments to make it work seamlessly and flexible with use of IAuthorizationService.

--- src/Omniwise.Application/Common/Interfaces/IUserContext.cs
+++ src/Omniwise.Application/Common/Interfaces/IUserContext.cs
@@ -1,8 +1,10 @@
 ﻿using Omniwise.Application.Common.Types;
+using System.Security.Claims;
 
 namespace Omniwise.Application.Common.Interfaces;
 
 public interface IUserContext
 {
+    ClaimsPrincipal? ClaimsPrincipalUser { get; }
     CurrentUser GetCurrentUser();
 }
--- src/Omniwise.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Omniwise.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Omniwise.Application.Common.Interfaces;
+using Omniwise.Domain.Constants;
 using Omniwise.Domain.Entities;
+using Omniwise.Infrastructure.Authorization.Requirements.SameOwner;
 using Omniwise.Infrastructure.Identity;
 using Omniwise.Infrastructure.Persistence;
 using Omniwise.Infrastructure.Persistence.MigrationAppliers;
@@ -35,5 +38,11 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IUserContext, UserContext>();
 
         services.AddScoped<ICoursesRepository, CoursesRepository>();
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy(Policies.SameOwner, policy =>
+                policy.Requirements.Add(new SameOwnerRequirement()));
+
+        services.AddSingleton<IAuthorizationHandler, SameOwnerRequirementHandler>();
     }
 }
