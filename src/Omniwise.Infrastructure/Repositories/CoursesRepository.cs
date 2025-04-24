56f3bd4366efdbcc57f9c99d44ad1ebd263e1699
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-04-24 12:22:56 +0200

    Feature: Created CoursesRepository with its interface in application layer and implemented CreateAsync method.

---
+++ src/Omniwise.Infrastructure/Repositories/CoursesRepository.cs
@@ -0,0 +1,21 @@
+﻿using Omniwise.Application.Common.Interfaces;
+using Omniwise.Domain.Entities;
+using Omniwise.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniwise.Infrastructure.Repositories;
+
+internal class CoursesRepository(OmniwiseDbContext dbContext) : ICoursesRepository
+{
+    public async Task<int> CreateAsync(Course course)
+    {
+        dbContext.Courses.Add(course);
+        await dbContext.SaveChangesAsync();
+
+        return course.Id;
+    }
+}
